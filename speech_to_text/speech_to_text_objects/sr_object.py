06965fb3bfb7b891f07bf366f9c42c450aff160d
oron.pariente <oron.pariente@stargo.co>
2024-09-08 18:09:47 +0300

    Init

---
+++ speech_to_text/speech_to_text_objects/sr_object.py
@@ -0,0 +1,49 @@
+import speech_recognition as sr
+from pydantic import Field, ConfigDict
+from speech_to_text.speech_to_text_objects.stt_base_object import STTObject
+import logging
+logger = logging.getLogger("SR Object")
+
+
+class SRObject(STTObject):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    recognizer: sr.Recognizer = Field(default=sr.Recognizer(), description="The recognizer object")
+
+    def recognize_speech_from_microphone(self):
+
+        # Use the microphone as source for input.
+        with sr.Microphone() as source:
+            logger.info("Adjusting for ambient noise, please wait...")
+
+            # Optional: Adjust the recognizer sensitivity to ambient noise (best practice in noisy environments)
+            self.recognizer.adjust_for_ambient_noise(source=source)
+
+            logger.info("Ready to listen, please speak.")
+
+            try:
+                # Capture the audio from the microphone
+                audio = self.recognizer.listen(source, timeout=5, phrase_time_limit=10)
+
+                # Use Google's Speech Recognition to convert audio to text
+                logger.info("Processing...")
+                recognized_text = self.recognizer.recognize_google_cloud(audio, language=self.language)
+                logger.info(f"You said: {recognized_text}")
+                return recognized_text
+
+            except sr.UnknownValueError:
+                # Speech was unintelligible
+                logger.error("Sorry, I could not understand the audio.")
+                return None
+            except sr.RequestError as e:
+                # Could not request results from Google Speech Recognition service
+                logger.error(f"Could not request results; {e}")
+                return None
+            except sr.WaitTimeoutError:
+                # User did not speak in time
+                logger.error("Listening timed out while waiting for phrase to start.")
+                return None
+            except Exception as e:
+                logger.error(f"An error occurred: {e}")
+                return None
+
---
+++ utils/logger.py
@@ -0,0 +1,42 @@
+# TODO: add the option to add params to logs.
+import logging
+import sys
+from utils.color_formatter import ColorFormatter
+
+
+class JarvisLogger:
+
+    def __init__(self, logger_name: str):
+        super().__init__()
+        # Create a custom logger
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)  # Set the minimum log level
+
+        # Create handlers
+        console_handler = logging.StreamHandler(sys.stdout)  # Log to console
+
+        # Set level for handlers
+        console_handler.setLevel(logging.DEBUG)
+
+        # Apply colored formatter for console logs
+        color_formatter = ColorFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(color_formatter)
+
+        # Apply normal formatter for file logs
+
+        # Add handlers to the logger
+        self.logger.addHandler(console_handler)
+
+    def info(self, message: str, params: dict = None):
+        self.logger.info(message)
+
+    def error(self, message: str, params: dict = None):
+        self.logger.error(message)
+
+
+if __name__ == '__main__':
+    logger = JarvisLogger(logger_name="test")
+    logger.info("hello world")
+    logger.error("this is an error")
+
+
