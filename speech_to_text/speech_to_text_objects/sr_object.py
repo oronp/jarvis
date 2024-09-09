4ba84157094ca61e33af6f567a28c6db194a20b2
oron.pariente <oron.pariente@stargo.co>
2024-09-09 18:48:36 +0300

    Update/ passive listener

--- speech_to_text/speech_to_text_objects/sr_object.py
+++ speech_to_text/speech_to_text_objects/sr_object.py
@@ -1,8 +1,11 @@
-import io
+import json
 import os
-from google.cloud import speech
+import queue
+
+import sounddevice as sd
 import speech_recognition as sr
 from pydantic import Field, ConfigDict
+from vosk import Model, KaldiRecognizer
 
 from speech_to_text.speech_to_text_objects.stt_base_object import STTObject
 from utils.logger import JarvisLogger
@@ -13,18 +16,25 @@ logger = JarvisLogger("SR_Object")
 class SRObject(STTObject):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    recognizer: sr.Recognizer = Field(default=sr.Recognizer(), description="The recognizer object")
+    recognizer: sr.Recognizer = Field(default_factory=sr.Recognizer, description="The recognizer object")
     credentials_json: dict = Field(default=os.path.join('config', 'jarvis-435015-e56d7f72b57b.json'),
                                    description="The credentials JSON for Google Cloud Speech-to-Text API")
+    vosk_model_path: str = Field(default=os.path.join('models', 'vosk-model-small-en-us-0.15'),
+                                 description="The path to the VOSK model")
+    sound_sample_rate: int = Field(default=16000, description="The sound sample rate")
+    language: str = Field(default="en-US", description="The language for speech recognition")
+    q: queue.Queue = Field(default_factory=queue.Queue, description="The queue for audio data")
 
-    # @classmethod
-    # @field_validator("credentials_json", mode='before')
-    # def validate_credentials_json(cls, value):
-    #     try:
-    #         value = json.loads('config/jarvis-435015-e56d7f72b57b.json')
-    #         return value
-    #     except (FileNotFoundError, json.JSONDecodeError):
-    #         return {}
+    vosk_model: Model = Field(default=None, init=False)
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.vosk_model = Model(self.vosk_model_path)
+
+    def queue_callback(self, indata, frames, time, status):
+        if status:
+            logger.info(status)
+        self.q.put(bytes(indata))
 
     def recognize_speech_with_google_api(self):
         with sr.Microphone() as source:
@@ -59,3 +69,22 @@ class SRObject(STTObject):
             except Exception as e:
                 logger.error(f"An error occurred: {e}")
                 return None
+
+    def passive_listener(self):
+        with sd.RawInputStream(samplerate=self.sound_sample_rate, blocksize=16000, dtype='int16', channels=1,
+                               callback=self.queue_callback):
+            logger.info("Listening... Press Ctrl+C to stop.")
+            rec = KaldiRecognizer(self.vosk_model, self.sound_sample_rate)
+
+            while True:
+                data = self.q.get()
+                if rec.AcceptWaveform(data):
+                    result = rec.Result()
+                    text = json.loads(result)["text"]
+                    if text:
+                        logger.info(f"Recognized: {text}")
+                        if "hello" in text.lower():
+                            self.recognize_speech_with_google_api()
+                            break
+                else:
+                    logger.info(rec.PartialResult())
