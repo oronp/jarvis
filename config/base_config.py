d34f57382f10e15a4df59c40d93d07a48b2c4a3d
oron.pariente <oron.pariente@stargo.co>
2024-09-14 17:36:36 +0300

    Update/openai credentials with config

--- config/base_config.py
+++ config/base_config.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 from utils.logger import JarvisLogger
 
 logger = JarvisLogger("BaseConfig")
@@ -7,15 +8,20 @@ logger = JarvisLogger("BaseConfig")
 
 class BaseConfig:
     google_json_name: str = "jarvis-435015-e56d7f72b57b.json"
+    openai_json_name: str = "open_ai.json"
+
     google_credentials_json_path: dict
+    openai_credentials_json_path: dict
 
     def __init__(self):
-        self.get_google_credentials_json_path()
+        self.set_credentials_json_path()
+        self.set_credentials_json_path()
 
     @classmethod
-    def get_google_credentials_json_path(cls):
+    def set_credentials_json_path(cls):
         config_dir = os.path.dirname(os.path.abspath(__file__))
         cls.google_credentials_json_path = os.path.join(config_dir, cls.google_json_name)
+        cls.openai_credentials_json_path = os.path.join(config_dir, cls.openai_json_name)
 
     @classmethod
     def get_google_credentials_json_as_dict(cls):
@@ -23,4 +29,3 @@ class BaseConfig:
             return json.loads(cls.google_credentials_json_path)
         except json.JSONDecodeError as e:
             logger.error("Failed to decode JSON from google_credentials_json", params=e)
-
